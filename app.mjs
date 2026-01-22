a4f01d1a7d1b2cb0e371ece057bcfb17692e8fbf
Nasser <znasser@una.edu>
2026-01-22 13:24:44 -0600

    update app and json

--- app.mjs
+++ app.mjs
@@ -1,11 +1,25 @@
-import express from 'express'
 
-const app = express()
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import express from 'express';
 
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// middlewares aka endpoints aka "get to slash" {http verb} to slash {you name your endpoint}
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  //res.send('Hello Express'); //string response
+  //res.sendFile('index.html');
+  res.sendFile(join(__dirname, 'public', 'index.html'));
+})
+
+
+app.get('/json', (req, res) => {
+  const myVar = 'Hello from server!';
+  res.json({ myVar });
 })
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log('Server is running on http://localhost:3000');
 })
