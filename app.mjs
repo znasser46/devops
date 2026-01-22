d7b7b90a2ad71699448fc9833db0230aa674edc3
Nasser <znasser@una.edu>
2026-01-22 13:48:29 -0600

    add app and index

--- app.mjs
+++ app.mjs
@@ -7,6 +7,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.json()); 
+
 // middlewares aka endpoints aka "get to slash" {http verb} to slash {you name your endpoint}
 app.get('/', (req, res) => {
   //res.send('Hello Express'); //string response
@@ -15,11 +17,28 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/json', (req, res) => {
+app.get('/api/json', (req, res) => {
   const myVar = 'Hello from server!';
   res.json({ myVar });
+  // <a href="json/">json</a>
 })
+
+app.get('/api/query', (req, res) => {
+
+  console.log("client request with query param:", req.query.name); 
+  res.json({"name": req.query.name});
+});
+
+app.get('/api/body', (req, res) => {
+
+  console.log("client request with body:", req.body);
+  console.log("client request with body:", req.body.name); 
+  res.json({"name": req.body.name});
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 })
+
+
